f4dcd591e1a6547bbb745e9d799644e9fbf75e4d
hxm <hxm@hxmdeiMac.local>
2016-05-30 03:34:44 +0800

    Added dm_zeros(),dm_ones(),dm_seqs(),dm_eyes(), assigment '='(dm_copy), operator '+'(dm_add).

--- dm.F90
+++ dm.F90
@@ -4,10 +4,19 @@
 module dm 
     use dm_type
     implicit none
+
     interface assignment(=)
         module procedure dm_copyEx
         module procedure dm_copyIm 
     end interface
+
+    interface operator (+)
+        module procedure dm_add1
+        module procedure dm_add2
+        module procedure dm_add3
+        module procedure dm_add4
+    end interface
+
 
     interface dm_destroy
         module procedure dm_destroyEx 
@@ -116,7 +125,7 @@ function dm_create(m,n) result(A)
 #include <petsc/finclude/petscvec.h90>
 #include <petsc/finclude/petscmat.h>
     PetscInt,       intent(in)  ::  m,n 
-    type(MatrixIm)                ::  A
+    type(MatrixIm)              ::  A
     PetscErrorCode              ::  ierr
     ! generate matrix A with size m*n
     call MatCreate(PETSC_COMM_WORLD,A%x,ierr);
@@ -168,10 +177,8 @@ function dm_view(A) result(ierr)
 #include <petsc/finclude/petscmat.h>
     type(Matrix),  intent(in)   ::  A
     PetscErrorCode              ::  ierr
-    ! veiw matrix A
-    call MatAssemblyBegin(A%x,MAT_FINAL_ASSEMBLY,ierr)
-    call MatAssemblyEnd(A%x,MAT_FINAL_ASSEMBLY,ierr)
-
+	call MatAssemblyBegin(A%x,MAT_FINAL_ASSEMBLY,ierr)
+	call MatAssemblyEnd(A%x,MAT_FINAL_ASSEMBLY,ierr)
     call MatView(A%x,PETSC_VIEWER_STDOUT_WORLD, ierr)
 end function 
 
@@ -187,10 +194,17 @@ function dm_zeros(m,n) result(A)
     PetscInt,       intent(in)  ::  m,n 
     type(MatrixIm)                ::  A
     PetscErrorCode              ::  ierr
+	PetscLogEvent	            ::  ievent
 
-    !call mat_create(A,m,n,ierr)
+	call PetscLogEventRegister("dm_zeros",0, ievent, ierr)
+    call PetscLogEventBegin(ievent,ierr)
+    
     A=dm_create(m,n)
     call MatZeroEntries(A%x,ierr)
+    
+    call MatAssemblyBegin(A%x,MAT_FINAL_ASSEMBLY,ierr)
+    call MatAssemblyEnd(A%x,MAT_FINAL_ASSEMBLY,ierr)
+    call PetscLogEventEnd(ievent,ierr)
 end function
 
 
@@ -208,14 +222,16 @@ function dm_ones(m,n) result(A)
     PetscInt,allocatable        ::  idxn(:)
     PetscScalar,allocatable     ::  row(:),results(:)
     integer                     ::  i,j
+	PetscLogEvent	            ::  ievent
 
+	call PetscLogEventRegister("dm_ones",0, ievent, ierr)
+    call PetscLogEventBegin(ievent,ierr)
+    
     A=dm_create(m,n)
     call MatGetOwnershipRange(A%x,ista,iend,ierr)
     !print *,">ista=",ista,"iend=",iend,">ncol=",ncol
     allocate(idxn(n),row(n),results(n))
 
-    !call MatAssemblyBegin(T,MAT_FINAL_ASSEMBLY,ierr)
-    !call MatAssemblyEnd(T,MAT_FINAL_ASSEMBLY,ierr)
     do i=ista,iend-1
         do j=1,n
             idxn(j)=j-1
@@ -227,37 +243,244 @@ function dm_ones(m,n) result(A)
     call MatAssemblyEnd(A%x,MAT_FINAL_ASSEMBLY,ierr)
 
     deallocate(idxn,row,results)
+    
+    call PetscLogEventEnd(ievent,ierr)
 end function
+
+
+! -----------------------------------------------------------------------
+! A=[1 2 3], This function is only used to generate the test data.
+!   [4 5 6]
+!   [7 8 9]
+! -----------------------------------------------------------------------
+function dm_seqs(m,n) result(A)
+    implicit none
+#include <petsc/finclude/petscsys.h>
+#include <petsc/finclude/petscvec.h>
+#include <petsc/finclude/petscvec.h90>
+#include <petsc/finclude/petscmat.h>
+    PetscInt,       intent(in)  ::  m,n
+    PetscErrorCode              ::  ierr
+    type(MatrixIm)                ::  A
+
+    PetscInt                    ::  ista,iend
+    PetscInt,allocatable        ::  idxn(:)
+    PetscScalar,allocatable     ::  row(:),results(:)
+    integer                     ::  i,j
+	PetscLogEvent	            ::  ievent
+
+	call PetscLogEventRegister("dm_seq",0, ievent, ierr)
+    call PetscLogEventBegin(ievent,ierr)
+    
+    A=dm_create(m,n)
+    call MatGetOwnershipRange(A%x,ista,iend,ierr)
+    !print *,">ista=",ista,"iend=",iend,">ncol=",ncol
+    allocate(idxn(n),row(n),results(n))
+
+    do i=ista,iend-1
+        do j=1,n
+            idxn(j)=j-1
+			row(j)=i*n+j
+        enddo
+        call MatSetValues(A%x,1,i,n,idxn,row,INSERT_VALUES,ierr)
+    enddo
+    call MatAssemblyBegin(A%x,MAT_FINAL_ASSEMBLY,ierr)
+    call MatAssemblyEnd(A%x,MAT_FINAL_ASSEMBLY,ierr)
+
+    deallocate(idxn,row,results)
+    
+    call PetscLogEventEnd(ievent,ierr)
+end function
+
+
+! -----------------------------------------------------------------------
+! The eyes function is used to generate the simple and complex identity matrixs. 
+! For example, if A is a 2*6 matrix, we can use mat_eye(A,ierr) to obtain 
+! A= [1 0 1 0 1 0]
+!	 [0 1 0 1 1 0]
+! if A is a 6*2 matrix, then mat_eye(A,ierr) will generate
+! A= [1 0]
+!	 [0 1]
+!	 [1 0]
+!    [0 1]
+!	 [1 0]
+!    [0 1]
+! -----------------------------------------------------------------------
+function dm_eyes(m,n) result(A)
+	implicit none
+#include <petsc/finclude/petscsys.h>
+#include <petsc/finclude/petscvec.h>
+#include <petsc/finclude/petscvec.h90>
+#include <petsc/finclude/petscmat.h>
+	PetscInt,       intent(in)	::	m,n	
+	type(MatrixIm)			    ::	A
+	PetscErrorCode	            ::	ierr
+	PetscInt					::	nmax, nmin 
+	PetscInt					::  ista,iend,xpos,ypos
+	PetscInt,allocatable		::	idxn(:)
+	PetscScalar,allocatable		::	row(:)
+	integer 					:: 	i,j,counter
+	PetscLogEvent	            ::  ievent
+
+	call PetscLogEventRegister("dm_eye",0, ievent, ierr)
+    call PetscLogEventBegin(ievent,ierr)
+    
+    A=dm_create(m,n) 
+    nmin=min(m,n)
+	nmax=max(m,n)
+ 	if(mod(nmax,nmin) /= 0) then
+		print *, "Error in dm_eye: the size of matrix A should be (NM)*M or M*(NM)"
+		stop	
+	endif
+	call MatGetOwnershipRange(A%x,ista,iend,ierr)
+	allocate(idxn(nmax/nmin),row(nmax/nmin))
+
+    do i=ista,iend-1
+    	xpos=mod(i,nmin)
+
+        counter=0	
+		do j=1,n
+    		ypos=mod(j-1,nmin)
+    		if(ypos==xpos) then
+                counter=counter+1
+    			row(counter)=1.0
+			    idxn(counter)=j-1
+    		    !print *,"i=",i,"j=",j,"xpos=",xpos,"ypos=",ypos,"idxn(",counter,")=",idxn(counter),"row(",j,")=",row(counter) 
+    		endif
+		enddo
+	   	call MatSetValues(A%x,1,i,counter,idxn,row,INSERT_VALUES,ierr)
+	enddo
+    call MatAssemblyBegin(A%x,MAT_FINAL_ASSEMBLY,ierr)
+    call MatAssemblyEnd(A%x,MAT_FINAL_ASSEMBLY,ierr)
+	deallocate(idxn,row)
+    call PetscLogEventEnd(ievent,ierr)
+end function 
+
+
 
 ! -----------------------------------------------------------------------
 ! B=A. This function uses the implicit matrix A directly because A is not need to free. 
 ! -----------------------------------------------------------------------
 subroutine dm_copyIm(B,A)
     implicit none
-
 #include <petsc/finclude/petscsys.h>
 #include <petsc/finclude/petscvec.h>
 #include <petsc/finclude/petscvec.h90>
 #include <petsc/finclude/petscmat.h>
-    type(Matrix),            intent(out) ::  B
-    type(MatrixIm),            intent(in)  ::  A
+    type(MatrixIm),  intent(in)  ::  A
+    type(Matrix),    intent(out) ::  B
+    PetscErrorCode               ::  ierr
+    !Free the space of B matrix 
+    !call MatDestroy(B%x,ierr)
     B%x=A%x
 end subroutine
 
 
 subroutine dm_copyEx(B,A)
     implicit none
-
 #include <petsc/finclude/petscsys.h>
 #include <petsc/finclude/petscvec.h>
 #include <petsc/finclude/petscvec.h90>
 #include <petsc/finclude/petscmat.h>
-    type(Matrix),            intent(out) ::  B
-    type(Matrix),            intent(in)  ::  A
+    type(Matrix),   intent(in)  ::  A
+    type(Matrix),   intent(out) ::  B
     PetscErrorCode              ::  ierr
-    
+    !Free the space of B matrix 
+    !call MatDestroy(B%x,ierr)
     call MatDuplicate(A%x,MAT_COPY_VALUES,B%x,ierr)
 end subroutine
+
+
+
+! -----------------------------------------------------------------------
+! C=A+B
+! -----------------------------------------------------------------------
+subroutine mat_add(A,B,C,ierr) 
+	implicit none
+#include <petsc/finclude/petscsys.h>
+#include <petsc/finclude/petscvec.h>
+#include <petsc/finclude/petscvec.h90>
+#include <petsc/finclude/petscmat.h>
+	Mat,	       intent(in)	::  A 
+	Mat,	       intent(in)	::  B 
+	Mat,           intent(out)  ::	C
+	PetscErrorCode,intent(out)  ::	ierr
+	PetscInt					::	nrow1,ncol1,nrow2,ncol2
+    PetscScalar                 ::  alpha	
+    call MatGetSize(A,nrow1,ncol1,ierr)
+	call MatGetSize(B,nrow2,ncol2,ierr)
+	if(nrow1/=nrow2 .or. ncol1/=ncol2)then
+		print *, "Error: matrix A and matrix B should have the same size"
+		stop	
+	endif
+    
+    alpha=1.0
+    call MatDuplicate(A,MAT_COPY_VALUES,C,ierr)
+	call MatAXPY(C,alpha,B,DIFFERENT_NONZERO_PATTERN,ierr)
+end subroutine 
+
+
+function dm_add1(A,B) result(C)
+	implicit none
+#include <petsc/finclude/petscsys.h>
+#include <petsc/finclude/petscvec.h>
+#include <petsc/finclude/petscvec.h90>
+#include <petsc/finclude/petscmat.h>
+	type(Matrix),	intent(in)	::  A 
+	type(Matrix),	intent(in)	::  B 
+	type(MatrixIm)              ::	C
+	PetscErrorCode      		::	ierr
+    call mat_add(A%x,B%x,C%x,ierr)
+end function 
+
+
+function dm_add2(A,B) result(C)
+	implicit none
+#include <petsc/finclude/petscsys.h>
+#include <petsc/finclude/petscvec.h>
+#include <petsc/finclude/petscvec.h90>
+#include <petsc/finclude/petscmat.h>
+	type(MatrixIm),	intent(in)	::  A 
+	type(MatrixIm),	intent(in)	::  B 
+	type(MatrixIm)              ::	C
+	PetscErrorCode      		::	ierr
+    call mat_add(A%x,B%x,C%x,ierr)
+    call MatDestroy(A%x,ierr)
+    call MatDestroy(B%x,ierr)
+end function 
+
+
+function dm_add3(A,B) result(C)
+	implicit none
+#include <petsc/finclude/petscsys.h>
+#include <petsc/finclude/petscvec.h>
+#include <petsc/finclude/petscvec.h90>
+#include <petsc/finclude/petscmat.h>
+	type(MatrixIm),	intent(in)	::  A 
+	type(Matrix),	intent(in)	::  B 
+	type(MatrixIm)              ::	C
+	PetscErrorCode      		::	ierr
+    call mat_add(A%x,B%x,C%x,ierr)
+    call MatDestroy(A%x,ierr)
+end function 
+
+
+function dm_add4(A,B) result(C)
+	implicit none
+#include <petsc/finclude/petscsys.h>
+#include <petsc/finclude/petscvec.h>
+#include <petsc/finclude/petscvec.h90>
+#include <petsc/finclude/petscmat.h>
+	type(Matrix),	intent(in)	::  A 
+	type(MatrixIm),	intent(in)	::  B 
+	type(MatrixIm)              ::	C
+	PetscErrorCode      		::	ierr
+    call mat_add(A%x,B%x,C%x,ierr)
+    call MatDestroy(B%x,ierr)
+end function 
+
+
+
 
 
 end module 
--- main.F90
+++ main.F90
@@ -9,7 +9,7 @@ program main
 #include <petsc/finclude/petscmat.h>
 #include <petsc/finclude/petscksp.h>
 
-    type(Matrix)    :: A,B    
+    type(Matrix)    :: A,B,C,D,E,F,G,H 
     integer         :: myrank, mysize 
     integer         :: m,n 
     real(kind=8)    :: ep
@@ -26,17 +26,118 @@ program main
     n=dm_get_int('-n')
     ep=dm_get_real('-ep')
     debug=dm_get_bool('-debug')
-    print *,m,n,ep,debug
     
-    A=dm_ones(4,3)
+    if(myrank==0) then 
+       print *, "============Input paramenters============"
+        print *, "m=",m,",n=",n,"ep=",ep,"debug=",debug
+     endif 
+	
     
-    B = A
-
-    ierr=dm_view(A)
-    ierr=dm_view(B)
-
+ 	if(myrank==0) print *, "==============Test dm_zeros==============="
+    A=dm_zeros(m,n)
+    if(debug) then
+        if(myrank==0) print *, ">A="
+        ierr=dm_view(A)
+ 	endif
     ierr=dm_destroy(A)
-    ierr=dm_destroy(B)
+
+
+ 	if(myrank==0) print *, "==============Test dm_ones================"
+    A=dm_ones(m,n)
+    if(debug) then
+        if(myrank==0) print *, ">A="
+        ierr=dm_view(A)
+ 	endif
+    ierr=dm_destroy(A)
+
+ 	if(myrank==0) print *, "==============Test dm_seq================"
+    A=dm_seqs(m,n)
+    if(debug) then
+        if(myrank==0) print *, ">A="
+        ierr=dm_view(A)
+ 	endif
+    ierr=dm_destroy(A)
+
+
+    if(myrank==0) print *, "==============Test dm_eye==============="
+    A=dm_eyes(m,m)	
+    if(debug) then
+        if(myrank==0) print *, ">A=(m,m)"
+        ierr=dm_view(A)
+ 	endif
+ 	ierr=dm_destroy(A)
+
+    A=dm_eyes(m,m*2)	
+    if(debug) then
+        if(myrank==0) print *, ">A=(m,2m)"
+        ierr=dm_view(A)
+ 	endif
+ 	ierr=dm_destroy(A)
+
+    A=dm_eyes(2*m,m)	
+    if(debug) then
+        if(myrank==0) print *, ">A=(2m,m)"
+        ierr=dm_view(A)
+ 	endif
+ 	ierr=dm_destroy(A)
+
+
+ 	if(myrank==0) print *, "==============Test dm_copy==============="
+ 	A=dm_ones(m,m)
+    B=A
+    if(debug) then
+        if(myrank==0) print *, ">A="
+        ierr=dm_view(A)
+        if(myrank==0) print *, ">B="
+        ierr=dm_view(B)
+ 	endif
+ 	ierr=dm_destroy(A)
+ 	ierr=dm_destroy(B)
+
+
+    if(myrank==0) print *, "==============Test dm_add==============="
+ 	A=dm_eyes(m,m)
+    B=dm_ones(m,m)
+    !C=dm_add1(A,B)
+    C=A+B
+    D=dm_eyes(m,m)+dm_ones(m,m)
+    E=dm_eyes(m,m)+B
+    F=A+dm_ones(m,m)
+    G=A+A+A
+    H=B+G
+    if(debug) then
+        if(myrank==0) print *, ">A="
+        ierr=dm_view(A)
+        if(myrank==0) print *, ">B="
+        ierr=dm_view(B)
+        if(myrank==0) print *, ">C="
+        ierr=dm_view(C)
+        if(myrank==0) print *, ">D="
+        ierr=dm_view(D)
+        if(myrank==0) print *, ">E="
+        ierr=dm_view(E)
+        if(myrank==0) print *, ">F="
+        ierr=dm_view(F)
+        if(myrank==0) print *, ">G="
+        ierr=dm_view(G)
+        if(myrank==0) print *, ">H="
+        ierr=dm_view(H)
+ 	endif
+ 	ierr=dm_destroy(A)
+ 	ierr=dm_destroy(B)
+ 	ierr=dm_destroy(C)
+ 	ierr=dm_destroy(D)
+ 	ierr=dm_destroy(E)
+ 	ierr=dm_destroy(F)
+ 	ierr=dm_destroy(G)
+ 	ierr=dm_destroy(H)
+
+
+
+
+
+
+
 
     call PetscFinalize(ierr)
 end program
